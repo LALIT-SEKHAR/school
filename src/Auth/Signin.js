7369f4bcadd52f119dd17cd2274b7ed69f9fc696
LALIT-SEKHAR <lalitsekhar1999@gmail.com>
2020-07-24 21:56:13 +0530

    added signin and signup astethic

--- src/Auth/Signin.js
+++ src/Auth/Signin.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signin = () => {
     return (
-        <div>
-            <h1>Signin</h1>
+        <div className='authSection'>
+            <div className="imgSection">
+                <img src="favicon.png" alt="Logo"/>
+            </div>
+            <div className="inputSection">
+                <form className="authForm">
+                    <h2 className="authheading">SIGN IN</h2>
+                    <label className="authInputTextLable" htmlFor="email">email</label><br/>
+                    <input placeholder="Enter Email" className="authInput" type="email"/><br/>
+                    <label className="authInputTextLable" htmlFor="password">password</label><br/>
+                    <input placeholder="Enter Password" className="authInput" type="password"/><br/>
+                    <span className="authSubmitSection"><Link className="chengAuthLink" to="/signup">not an user?</Link><button className="authSubmit">submit</button></span>
+                </form>
+            </div>
         </div>
     );
 }
--- src/Auth/Signup.js
+++ src/Auth/Signup.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './Auth.css';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
     return (
-        <div>
-            <h1>Signup</h1>
+        <div className='authSection'>
+            <div className="imgSection">
+                <img src="favicon.png" alt="Logo"/>
+            </div>
+            <div className="inputSection">
+                <form className="authForm">
+                    <h2 className="authheading">SIGN UP</h2>
+                    <label className="authInputTextLable" htmlFor="name">name</label><br/>
+                    <input placeholder="Enter Name" className="authInput" type="text"/><br/>
+                    <label className="authInputTextLable" htmlFor="email">email</label><br/>
+                    <input placeholder="Enter Email" className="authInput" type="email"/><br/>
+                    <label className="authInputTextLable" htmlFor="password">password</label><br/>
+                    <input placeholder="Enter Password" className="authInput" type="password"/><br/>
+                    <span className="authSubmitSection"><Link className="chengAuthLink" to="/signin">already a user?</Link><button className="authSubmit">submit</button></span>
+                </form>
+            </div>
         </div>
     );
 };
