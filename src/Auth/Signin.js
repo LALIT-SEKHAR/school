f9b518cc7db133f8a11d14dfddf0a827d6995ee0
LALIT-SEKHAR <lalitsekhar1999@gmail.com>
2020-07-25 23:12:08 +0530

    complit signin & signup

--- src/Auth/Signin.js
+++ src/Auth/Signin.js
@@ -1,20 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { signin } from './helper/authHelper';
 
 const Signin = () => {
+
+    const [value, setvalue] = useState({
+        email: '',
+        password: '',
+        isLoadimg: undefined,
+        dataResived: false,
+    });
+
+    const handalechange = (e) => {
+        setvalue({...value, [e.target.name] : e.target.value})
+    }
+
+    // const imgbgcolor = value.isSuccess ? {backgroundColor:'#6aff6a'} : !value.isSuccess ? {backgroundColor:'#ff6c6c'} : {backgroundColor:'#ffffff'}
+
     return (
         <div className='authSection'>
             <div className="imgSection">
-                <img src="favicon.png" alt="Logo"/>
+                <img className="authimg" src="favicon.png" alt="Logo"/>
             </div>
             <div className="inputSection">
                 <form className="authForm">
                     <h2 className="authheading">SIGN IN</h2>
                     <label className="authInputTextLable" htmlFor="email">email</label><br/>
-                    <input placeholder="Enter Email" className="authInput" type="email"/><br/>
+                    <input 
+                        name='email' 
+                        placeholder="Enter Email" 
+                        className="authInput" 
+                        type="email"
+                        onChange={handalechange}
+                    /><br/>
                     <label className="authInputTextLable" htmlFor="password">password</label><br/>
-                    <input placeholder="Enter Password" className="authInput" type="password"/><br/>
-                    <span className="authSubmitSection"><Link className="chengAuthLink" to="/signup">not an user?</Link><button className="authSubmit">submit</button></span>
+                    <input 
+                        name='password'
+                        placeholder="Enter Password" 
+                        className="authInput" 
+                        type="password"
+                        onChange={handalechange}
+                    /><br/>
+                    <span className="authSubmitSection">
+                        <Link className="chengAuthLink" to="/signup">
+                            not an user?
+                        </Link>
+                        <button onClick={async(e) => {
+                            e.preventDefault();
+                            const {email, password} = value
+                            if ( email !== '' && password !== '' ) {
+                                setvalue({...value,dataResived: true})
+                                await signin({email,password})
+                                .then((res)=>{
+                                    if (res.error) {
+                                        setvalue({...value, isSuccess: false})
+                                        return console.log("ERROR: ",res);
+                                    } else {
+                                        setvalue({...value, isSuccess: true})
+                                        return setvalue({
+                                            email:'',
+                                            password:'',
+                                        })
+                                    }
+                                })
+                                .catch((err)=>{console.log("ERROR in SIGNIN",err)})
+                                setvalue({...value,dataResived: false})
+                            }
+                            // console.log(name); 
+                        }} className="authSubmit"
+                        >
+                            {value.dataResived ? 'submiting..' : 'submit'}
+                        </button>
+                    </span>
                 </form>
             </div>
         </div>
--- src/Auth/Signup.js
+++ src/Auth/Signup.js
@@ -1,23 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Auth.css';
 import { Link } from 'react-router-dom';
+import { signup } from './helper/authHelper';
 
 const Signup = () => {
+
+    const [value, setvalue] = useState({
+        name: '',
+        email: '',
+        password: '',
+        isLoadimg: false,
+        dataResived: false,
+        datasubmitedSuccessfull:{backgroundColor:'#ffffff'},
+    });
+
+    const handalechange = (e) => {
+        setvalue({...value, [e.target.name] : e.target.value})
+    }
+
+    // const submitStatusSign = {
+    //     // border:'3px solid red',
+    //     datasubmitedSuccessfull 
+    //     ? 
+    //     backgroundColor:'#ff6c6c' 
+    //     : 
+    //     backgroundColor:'#6aff6a'
+    // }
+
     return (
         <div className='authSection'>
             <div className="imgSection">
-                <img src="favicon.png" alt="Logo"/>
+                <img style={value.datasubmitedSuccessfull} src="favicon.png" alt="Logo"/>
             </div>
             <div className="inputSection">
                 <form className="authForm">
                     <h2 className="authheading">SIGN UP</h2>
                     <label className="authInputTextLable" htmlFor="name">name</label><br/>
-                    <input placeholder="Enter Name" className="authInput" type="text"/><br/>
+                    <input 
+                        name="name"
+                        placeholder="Enter Name" 
+                        className="authInput" 
+                        type="text"
+                        value={value.name}
+                        onChange={handalechange}
+                    /><br/>
                     <label className="authInputTextLable" htmlFor="email">email</label><br/>
-                    <input placeholder="Enter Email" className="authInput" type="email"/><br/>
+                    <input 
+                        name="email"
+                        placeholder="Enter Email" 
+                        className="authInput" 
+                        type="email"
+                        value={value.email}
+                        onChange={handalechange}
+                    /><br/>
                     <label className="authInputTextLable" htmlFor="password">password</label><br/>
-                    <input placeholder="Enter Password" className="authInput" type="password"/><br/>
-                    <span className="authSubmitSection"><Link className="chengAuthLink" to="/signin">already a user?</Link><button className="authSubmit">submit</button></span>
+                    <input 
+                        name="password"
+                        placeholder="Enter Password" 
+                        className="authInput" 
+                        type="password"
+                        value={value.password}
+                        onChange={handalechange}
+                    /><br/>
+                    <span className="authSubmitSection">
+                        <Link className="chengAuthLink" to="/signin">
+                            already a user?
+                        </Link>
+                        <button onClick={async(e) => {
+                            e.preventDefault();
+                            const {name, email, password} = value
+                            if ( name !== '' && email !== '' && password !== '' ) {
+                                setvalue({...value,dataResived: true})
+                                await signup({name,email,password})
+                                .then((res)=>{
+                                    if (res.error) {
+                                        return console.log("ERROR: ",res);
+                                    } else {
+                                        return setvalue({
+                                            name:'',
+                                            email:'',
+                                            password:'',
+                                        })
+                                    }
+                                })
+                                .catch((err)=>{console.log("ERROR in SIGNUP",err)})
+                                setvalue({...value,dataResived: false})
+                            }
+                            // console.log(name); 
+                        }} className="authSubmit"
+                        >
+                            {value.dataResived ? 'submiting..' : 'submit'}
+                        </button>
+                    </span>
                 </form>
             </div>
         </div>
